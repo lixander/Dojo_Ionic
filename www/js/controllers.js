34b612a08caf021d9c2405ecba66e91da1bd8a82
lixander <lixandercad@gmail.com>
2015-11-07 09:58:50 -0500

    Se agregó un 'Cargar Más' al final de las publicaciones por medio de un infinite-scroll en el index

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,5 +15,27 @@ angular.module('starter.controllers',[])
 
     }); 
      
-        
+       
+	
+$scope.loadMorePost = function() {
+          var params = {};
+          if($scope.posts.length > 0) {
+
+            params['after'] = $scope.posts[$scope.posts.length - 1].name;
+            console.log(params);
+          }
+
+
+           $http.get('https://www.reddit.com/r/movies/new/.json',{params:params}).success(function(posts){
+            //console.log(posts);
+
+            angular.forEach(posts.data.children, function(post){
+
+              $scope.posts.push(post.data);
+              
+            });
+
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+      }; 
 }]);
